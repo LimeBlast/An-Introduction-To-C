70445153a8fc65f1da3b60a1fbd4834f1278f711
Daniel Hollands <daniel@limeblast.co.uk>
2016-08-30 21:30:39 +0100

    Add some conditional and loop examples from part 3

---
+++ part-03-conditions-and-comparissons/alt_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int a = 5;
+
+    // run at least once
+    do
+    {
+        printf ("a is equal to %d\n", a);
+        a++;
+    } while (a < 5);
+
+
+    printf ("a is equal to %d and I've finished\n", a);
+}
